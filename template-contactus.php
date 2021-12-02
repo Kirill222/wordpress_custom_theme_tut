34b2594d4b322939a10f8fe764241d7b46e22c60
johnconnor <zkmd2012@inbox.ru>
2021-12-02 14:27:58 +0500

    5 Nav menues

--- template-contactus.php
+++ template-contactus.php
@@ -8,21 +8,25 @@ Template Name: Contact us
 
 <?php get_header(); ?>
 
-<div class="container">
-    <h1><?php the_title(); ?></h1>
+<section class="page-wrap">
 
-    <div class="row">
-        <div class="col-lg-6">
-            this is where the contact form goes
+    <div class="container">
+        <h1><?php the_title(); ?></h1>
+
+        <div class="row">
+            <div class="col-lg-6">
+                this is where the contact form goes
+            </div>
+            <div class="col-lg-6">
+            <?php get_template_part('includes/section', 'content'); ?>
+            </div>
         </div>
-        <div class="col-lg-6">
+
+
         <?php get_template_part('includes/section', 'content'); ?>
-        </div>
+
     </div>
 
-
-    <?php get_template_part('includes/section', 'content'); ?>
-
-</div>
+</section>
 
 <?php get_footer(); ?>
